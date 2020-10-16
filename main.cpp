019d05ac3a3f960a83261a71d7296e909bf65886
yerson001 <yhon.sanchez@ucsp.edu.pe>
2020-10-16 15:58:48 -0500

    este es el tercer commit

--- main.cpp
+++ main.cpp
@@ -5,5 +5,8 @@ int main(){
  cout<<"este es el ejemplo para el git clone"<<endl;
  cout<<"esta es la prueva en git windowa"<<endl;
  cout<<"esta es la prueva 3 "<<endl;
+ for(int i=0; i<10; i++) {
+  cout<<i<<endl;
+ }
 return 0;
 }
