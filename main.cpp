bb0465197ca8e462711335fbb72f0581599ea53e
yerson001 <yhon.sanchez@ucsp.edu.pe>
2020-10-16 15:56:52 -0500

    hola

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main(){
  cout<<"este es el ejemplo para el git clone"<<endl;
  cout<<"esta es la prueva en git windowa"<<endl;
+ cout<<"esta es la prueva 3 "<<endl;
 return 0;
 }
