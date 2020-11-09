fbbd87b8a3699241c84a1bcea29a2789a27bd393
yerson001 <yhon.sanchez@ucsp.edu.pe>
2020-11-09 10:35:11 -0500

    integracioncin con git

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@ int main(){
  cout<<"este es el ejemplo para el git clone"<<endl;
  cout<<"esta es la prueva en git windowa"<<endl;
  cout<<"esta es la prueva 3 "<<endl;
+ cout<<"esta es otra forma de gamma_distribution"<<endl;
+ cout<<"para integrarlo con FloaternNER"<<endl;
+ cout<<"para integrarlo con FloaternNER"<<endl;
+ cout<<"para integrarlo con FloaternNER"<<endl;
+ cout<<"para integrarlo con FloaternNER"<<endl;
  for(int i=0; i<10; i++) {
   cout<<i<<endl;
  }
